e3ac035dfebf20b67301dfadad79b7e54171f6fa
Ella Tanttu <ellajtanttu@gmail.com>
2022-01-18 11:06:53 -0800

    successfully return message by id
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

---
+++ MessageBoard/Controllers/MessagesController.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MessageBoard.Models;
+
+namespace MessageBoard.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MessagesController : ControllerBase
+  {
+    private readonly MessageBoardContext _db;
+
+    public MessagesController(MessageBoardContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Message>> GetMessage(int id)
+    {
+      var message = await _db.Messages.FindAsync(id);
+
+      if (message == null)
+      {
+          return NotFound();
+      }
+
+      return message;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Message>> Post(Message message)
+    {
+      _db.Messages.Add(message);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = message.MessageId }, message);
+    }
+  }
+}
